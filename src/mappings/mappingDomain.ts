e384f343bceaae4a1bae387d24ea4cf4eab980e1
aoemaster <86769990+aoemaster@users.noreply.github.com>
2021-07-06 23:00:32 +0800

    add domainBindAddressEvent (#7)

--- src/mappings/mappingDomain.ts
+++ src/mappings/mappingDomain.ts
@@ -5,9 +5,8 @@ import {hexToUtf8} from '../helpers/common';
 import { Domain } from "../types/models/Domain";
 import type { ITuple } from '@polkadot/types/types';
 import type { ClassId } from '@polkadot/types/interfaces/uniques';
-import type { TokenId } from 'domain-types/src/interfaces/types';
+import type { TokenId, AddressChainType } from 'domain-types/src/interfaces/types';
 import { NFT } from "../types";
-
 
 //Self::deposit_event(Event::DomainRegistered(who, domain, ethereum, deposit, (T::NftClassID::get(), token_id.into())  ));
 export async function domainRegisterEvent(event: SubstrateEvent): Promise<void> {
@@ -19,7 +18,7 @@ export async function domainRegisterEvent(event: SubstrateEvent): Promise<void> 
     const deposit = (deposit_origin as Balance).toBigInt();
     const token0 = token0_origin as ITuple<[ClassId, TokenId]>;
 
-    let record = new Domain((domain_origin as Bytes).toString())
+    const record = new Domain((domain_origin as Bytes).toString())
     record.domain = domain;
     record.ownerId = who;
     record.ethereum = ethereum;
@@ -29,10 +28,10 @@ export async function domainRegisterEvent(event: SubstrateEvent): Promise<void> 
     await record.save();
 
     //save nft with domain
-    let domainInfo = await Domain.get((domain_origin as Bytes).toString());
+    const domainInfo = await Domain.get((domain_origin as Bytes).toString());
 
     if (domainInfo) {
-        let nft = new NFT(token0.toString())
+        const nft = new NFT(token0.toString())
         nft.classId = (token0[0] as u32).toNumber()
         nft.tokenId = (token0[1] as u64).toNumber()
         nft.domainInfoId = domainInfo.id;
@@ -45,9 +44,39 @@ export async function domainDeregisterEvent(event: SubstrateEvent): Promise<void
     const { event: { data: [who_origin, domain_origin] } } = event;
 
     const domain = (domain_origin as Bytes).toString();
-    let record = await Domain.get(domain);
+    const record = await Domain.get(domain);
     if (record) {
         record.registered = false;
         await record.save();
     }
+}
+
+// Self::deposit_event(Event::BindAddress(
+//     who,
+//     domain.clone(),
+//     chain_type,
+//     address,
+// ));
+export async function domainBindAddressEvent(event: SubstrateEvent): Promise<void> {
+    const { event: { data: [who_origin, domain_origin, chain_type_origin, address_origin] } } = event;
+    
+    const domain = (domain_origin as Bytes).toString();
+    const chain_type = chain_type_origin as AddressChainType;
+    const address = (address_origin as Bytes).toString();
+
+    const record = await Domain.get(domain);
+    if (record) {
+        if (chain_type.isBtc) {
+            record.bitcoin = address;
+        } else if(chain_type.isEth) {
+            record.ethereum = address
+        } else if(chain_type.isDot) {
+            record.polkadot = address
+        } else if(chain_type.isKsm) {
+            record.kusama = address
+        }
+
+        await record.save();
+    }
+
 }
